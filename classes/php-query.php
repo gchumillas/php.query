cc16e33a6ee81112cfa81904731f3d68f5e9210e
gonzalo.chumillas <gonzalo.chumillas@gmail.com>
2014-02-03 01:53:04 +0100

    capitalize classname

--- classes/php-query.php
+++ classes/php-query.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * This file contains the phpQuery class.
+ * This file contains the PhpQuery class.
  * 
  * PHP Version 5.3
  * 
  * @category XML
- * @package  phpQuery
+ * @package  PhpQuery
  * @author   Gonzalo Chumillas <gonzalo@soloproyectos.com>
  * @license  https://raw.github.com/soloproyectos/core/master/LICENSE BSD 2-Clause License
  * @link     https://github.com/soloproyectos/core
@@ -15,23 +15,23 @@ use com\soloproyectos\core\xml\query\XmlQuery;
 require_once __DIR__ . "/autoload.php";
 
 /**
- * Transitional phpQuery class.
+ * Transitional PhpQuery class.
  * 
  * This class does exactly the same as XmlQuery class, but I consider that
- * phpQuery is a more intuitive name for end users. Actually, the phpQuery class
+ * PhpQuery is a more intuitive name for end users. Actually, the PhpQuery class
  * forms part of a greater project. And, in the project context, XmlQuery is a good
  * name, because it indicates that the class is located under the ./xml/query
- * directory. But, outside of the context of the project, I think that phpQuery
+ * directory. But, outside of the context of the project, I think that PhpQuery
  * is a more intuitive name. That's the reason why blah blah blah. Do you like
  * bananas? I have a lot of bananas. Bananas bananas bananas...
  * 
  * @category XML
- * @package  phpQuery
+ * @package  PhpQuery
  * @author   Gonzalo Chumillas <gonzalo@soloproyectos.com>
  * @license  https://raw2.github.com/soloproyectos/php.common-libs/master/LICENSE BSD 2-Clause License
  * @link     https://github.com/soloproyectos/php.common-libs
  */
-class phpQuery extends XmlQuery
+class PhpQuery extends XmlQuery
 {
     
 }
